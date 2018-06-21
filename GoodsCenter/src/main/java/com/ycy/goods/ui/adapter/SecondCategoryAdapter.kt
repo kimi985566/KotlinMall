06e37d251c51289d0cc9e0e86f46952e9faf8c44
kimi985566 <chengyu_yang@outlook.com>
2018-06-21 09:19:37 +0800

    Category interface

--- GoodsCenter/src/main/java/com/ycy/goods/ui/adapter/SecondCategoryAdapter.kt
+++ GoodsCenter/src/main/java/com/ycy/goods/ui/adapter/SecondCategoryAdapter.kt
@@ -5,18 +5,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kotlin.base.ext.loadUrl
 import com.kotlin.base.ui.adapter.BaseRecyclerViewAdapter
 import com.ycy.goods.R
 import com.ycy.goods.data.protocol.Category
-import kotlinx.android.synthetic.main.layout_top_category_item.view.*
+import kotlinx.android.synthetic.main.layout_second_category_item.view.*
 
 /*
-    一级商品分类 Adapter
+    二级商品分类Adapter
  */
-class TopCategoryAdapter(context: Context) : BaseRecyclerViewAdapter<Category, TopCategoryAdapter.ViewHolder>(context) {
+class SecondCategoryAdapter(context: Context) : BaseRecyclerViewAdapter<Category, SecondCategoryAdapter.ViewHolder>(context) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(mContext)
-                .inflate(R.layout.layout_top_category_item, parent, false)
+                .inflate(R.layout.layout_second_category_item, parent, false)
 
         return ViewHolder(view)
     }
@@ -24,10 +25,10 @@ class TopCategoryAdapter(context: Context) : BaseRecyclerViewAdapter<Category, T
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         val model = dataList[position]
+        //分类图片
+        holder.itemView.mCategoryIconIv.loadUrl(model.categoryIcon)
         //分类名称
-        holder.itemView.mTopCategoryNameTv.text = model.categoryName
-        //是否被选中
-        holder.itemView.mTopCategoryNameTv.isSelected = model.isSelected
+        holder.itemView.mSecondCategoryNameTv.text = model.categoryName
 
     }
 
